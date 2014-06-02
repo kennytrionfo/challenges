f4c908a10c2f6b330ee57ce590b8b0335f52a0b2
kennytrionfo <kentrionfo@go@gmail.com>
2014-06-02 15:10:19 -0600

    pseudocode

---
+++ pseudocode.rb
@@ -0,0 +1,8 @@
+# Create a new file called pseudocode.rb to work on this exercise.
+# Write out the steps involved in making a tic-tac-toe app
+
+# Key points to cover:
+  # The board.
+  # Taking a turn.
+  # Determining a winner.
+  # Determining a tie.
