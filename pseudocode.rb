2b8115ab25fb1dbdea01bdc72b97abb88425a73c
kennytrionfo <kentrionfo@go@gmail.com>
2014-06-02 15:31:01 -0600

    more psuedo

--- pseudocode.rb
+++ pseudocode.rb
@@ -1,8 +1,21 @@
 # Create a new file called pseudocode.rb to work on this exercise.
 # Write out the steps involved in making a tic-tac-toe app
-
 # Key points to cover:
   # The board.
   # Taking a turn.
   # Determining a winner.
   # Determining a tie.
+
+Set a var to determine if still playing or not.
+Set var for number of turns.
+create a board to show. use an array to do so.
+Show the board.
+Ask for input for turn.
+take input. 
+insert it into the appropriate position in the array.
+Check to see if it's a win.
+If it's a win, print it, set still playing to false.
+If it's not a win, add 1 to turns, take my turn.
+See if my turn is a win.
+If so, print it, set still playing to false.
+If not, add one to turns, loop back up.
