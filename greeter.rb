c0d33961525fc99dd950cc7e4554b783497e0c88
kennytrionfo <kentrionfo@go@gmail.com>
2014-06-02 19:40:01 -0600

    just greeter

---
+++ greeter.rb
@@ -0,0 +1,29 @@
+# Create a new file called greeter.rb to work on this exercise.
+# Create a class called Greeter that is initialized with a string that is saved to the @greeting attribute.
+# Create a method that displays the string you initialized with to the console.
+# Create an instance of the Greeter class and call the display method on it.
+
+# Create a class called Greeter that is initialized with a string that is saved to the @greeting attribute.
+class Greeter
+  attr_accessor :greeting
+
+  # Create a method that displays the string you initialized with to the console.
+  def display
+    puts greeting
+  end
+
+  # Create an instance of the Greeter class and call the display method on it.
+  fred = Greeter.new
+  fred.display
+
+end
+
+
+
+
+  # Dave's example:
+  # attr_reader :greeting
+  #
+  # def initialize(greeting)
+  #   @greeting = greeting
+  # end
